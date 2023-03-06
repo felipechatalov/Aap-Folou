3254799fba7961f3961f064cd23156005f7e9894
Brumassio <dibrunspeed@gmail.com>
2023-03-06 16:33:36 -0300

    Consulta atualizada

--- frontend/src/views/ConsultaView.vue
+++ frontend/src/views/ConsultaView.vue
@@ -4,13 +4,16 @@
       <form @submit.prevent="submitForm">
         <div>
           <label for="name">Nome:</label>
-          <p>{{ user.nome }}</p>
+          <p>{{ user.name }}</p>
+          <label for="password">Senha:</label>
+          <p v-if="mostrar_senha">{{ user.password }}</p>
+          <p v-else>*********</p>          
           <label for="email">Email:</label>
           <p>{{ user.email }}</p>
           <label for="cpf">CPF:</label>
           <p>{{ user.cpf }}</p>
           <label for="phone">Telefone:</label>
-          <p>{{ user.telefone }}</p>
+          <p>{{ user.phone }}</p>
         </div>
         <button type="submit">Editar</button>
       </form>
@@ -21,13 +24,15 @@
 import dados from "@/assets/dados.json";
 
 export default {
-  data() {''
+  data() {
     return {
-      user: {}
+      user: {},
+      mostrar_senha: false
     }
   },
   created() {
     this.user = dados;
+    console.log(this.user.name);
   },
 };
 </script>
