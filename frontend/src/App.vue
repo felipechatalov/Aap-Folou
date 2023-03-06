917141bc1381b51ef60e9e44c993ddb80dc75ab2
Matheus <matheusmolina.mmd.mmd@gmail.com>
2023-03-06 02:12:10 -0300

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/consulta">Consulta</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
