6ce8e2edd0ba831378e11908baf0aaad30c85eae
Brumassio <dibrunspeed@gmail.com>
2023-03-14 10:04:38 -0300

    telas de edição bonitas

--- frontend/src/views/EditarPetshop.vue
+++ frontend/src/views/EditarPetshop.vue
@@ -1,19 +1,19 @@
 <template>
     <div>
         <form action="container">
-            <label for="nome"  placeholder="{{ dados.nome }}">Nome:</label>
-            <input type="text" v-model="novos_dados.nome">
-            <label for="password"  placeholder="**********">Senha:</label>
-            <input type="text" v-model="novos_dados.password">
+            <label for="nome">Nome:</label>
+            <input type="text" v-model="novos_dados.name" :placeholder="dados.name">
+            <label for="password">Senha:</label>
+            <input type="text" v-model="novos_dados.password" placeholder="**********">
             <label for="confirm_password">Confirmar Senha:</label>
             <input type="text" v-model="confirm_password">
             <span v-if="novos_dados.password !== confirm_password">As senhas não conferem</span>
-            <label for="email"  placeholder="{{ dados.email }}">Email:</label>
-            <input type="email" v-model="novos_dados.email">
-            <label for="cnpj"  placeholder="{{ dados.cnpj }}">CNPJ:</label>
-            <input type="text" v-model="novos_dados.cnpj">
-            <label for="phone"  placeholder="{{ dados.phone }}">Telefone:</label>
-            <input type="text" v-model="novos_dados.phone">
+            <label for="email">Email:</label>
+            <input type="email" v-model="novos_dados.email" :placeholder="dados.email">
+            <label for="cnpj">CNPJ:</label>
+            <input type="text" v-model="novos_dados.cnpj" :placeholder="dados.cnpj">
+            <label for="phone">Telefone:</label>
+            <input type="text" v-model="novos_dados.phone" :placeholder="dados.phone">
             <div class="botao">
                 <button @click.prevent="cancelar()">Cancelar</button>
                 <button @submit.prevent="editarDados()">Salvar</button>
@@ -23,10 +23,17 @@
 </template>
 
 <script>
+    import axios from 'axios';
     export default{
         data(){
             return{
-                dados: {},
+                dados: {
+                    name: 'Cássio Industries',
+                    password: '1234',
+                    email: 'cassiocorinthians@timao.com.br',
+                    cnpj: '02632646234',
+                    phone: '44921952152'
+                },
                 novos_dados: {},
                 confirm_password: ''
             }
