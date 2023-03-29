fb2a8893b6b5bee9df11d4df532333dac29d9ecf
Brumassio <dibrunspeed@gmail.com>
2023-03-29 11:32:24 -0300

    cabeçalho na altura correta, falta preencher toda a tela !

--- frontend/src/views/Navbar.vue
+++ frontend/src/views/Navbar.vue
@@ -4,8 +4,8 @@
       <router-link to="/" id="logo-url">
         <img :src="logo" :alt="alt" id="logo">
       </router-link> 
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">Pedidos</router-link>
+      <router-link to="/" id="router1">Home</router-link>
+      <router-link to="/about" id="router2">Pedidos</router-link>
     </div>
   </header>   
 </template>
@@ -17,25 +17,3 @@
     props: ["logo", "alt"]
   }
 </script>
-
-<style scoped>
-  #nav{
-    background-color:#6d553c;
-    border-bottom: 4px solid #111;
-    padding: 15px 50px;
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-    gap: 15px;
-    border: 0 auto;
-    padding: 0 auto;
-  }
-  #logo-url{
-    margin: auto;
-    margin-left: 0;
-  }
-
-  body{
-    min-height: 1000vh;
-  }
-</style>
