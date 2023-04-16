ebfa370c9bd51b3cc87f313db5e143b2bc358d1f
Brumassio <dibrunspeed@gmail.com>
2023-04-16 19:04:47 -0300

    Home criada, falta deixar responsivo !

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="home">
-        <h1>HOME</h1>
+        <div class="slogan">
+            <h1 class="aap_folou">AAP FOLOU</h1>
+            <h2>O que podemos fazer por eles hoje?</h2>
+        </div>
+
+        <div class="slogan_text">
+            <p>Conecte-se facilmente com petshops próximos de você através do nosso site e encontre 
+                a melhor opção para cuidar do seu animal de estimação! Encontre a melhor equipe de profissionais e serviços especializados em cuidados para o seu pet com apenas alguns cliques em nosso site. Descubra agora mesmo como podemos ajudar você e seu animal de estimação a terem a melhor experiência possível!</p>
+            <img src="../components/icons/home.png" alt="Home_Dogs">
+        </div>
     </div>
 </template>
 
--- frontend/src/views/Navbar.vue
+++ frontend/src/views/Navbar.vue
@@ -4,8 +4,12 @@
       <router-link to="/" id="logo-url">
         <img :src="logo" :alt="alt" id="logo">
       </router-link> 
-      <router-link to="/" id="router1">Home</router-link>
+      <a id="router1" href="" @click.prevent="mudarotaHome()">Home</a>
+      <a id="router2" href="" @click.prevent="mudarotaPedidos()">Pedidos</a>
+      <a id="router3" href="" @click.prevent="mudarotaLogin()">Login</a>
+      <!-- <router-link to="/" id="router1">Home</router-link>
       <router-link to="/about" id="router2">Pedidos</router-link>
+      <router-link to="/login" id="router3">Login</router-link> -->
     </div>
   </header>   
 </template>
@@ -14,6 +18,18 @@
   export default{
     // eslint-disable-next-line vue/multi-word-component-names
     name: "Navbar",
-    props: ["logo", "alt"]
+    props: ["logo", "alt"],
+
+    methods:{
+      mudarotaHome() {
+            this.$router.push("/");
+        },
+        mudarotaPedidos() {
+            this.$router.push("/about");
+        },
+        mudarotaLogin() {
+            this.$router.push("/login");
+        }
+    }
   }
 </script>
